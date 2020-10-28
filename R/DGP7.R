9e95d341a868298d096e93f4db82bdc185553b9e
颜冠鹏 <8192942+GuanpengYan@user.noreply.gitee.com>
2020-10-28 13:27:16 +0800

    20201028

--- R/DGP7.R
+++ R/DGP7.R
@@ -10,16 +10,16 @@ dataGeneratingProcess7 <- function(N, T0, T1){
   f <- array(dim = c(3, T))
   for(t in 1:T){
     if(t == 1){
-      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[2, t] <- zeta[2, t]
+      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[3, t] <- zeta[3, t]
     }else if(t == 2){
-      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[2, t] <- 0.5 * f[2, t - 1] + zeta[2, t] + 0.7 * zeta[2, t - 1]
+      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[3, t] <- zeta[3, t] + 0.9 * zeta[3, t - 1]
     }else{
-      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[2, t] <- 0.5 * f[2, t-1] + zeta[2, t] + 0.7 * zeta[2, t - 1]
+      f[1, t] <- 0.8 * f[2, t] + zeta[1, t]
       f[3, t] <- zeta[3, t] + 0.9 * zeta[3, t - 1] + 0.4 *zeta[3, t - 2]
     }
   }
@@ -29,7 +29,7 @@ dataGeneratingProcess7 <- function(N, T0, T1){
   x <- array(dim = c(K, N, T))
   for(t in 1:T) for(i in 1:N) for(k in 1:K) x[k, i, t] <- ifelse(
     t == 1,
-    1 + sum(c[, i] * f[, t])+eta[k, i, t],
+    1 + sum(c[, i] * f[, t]) + eta[k, i, t],
     1 + rho[k, i] * x[k, i, t - 1] + c[1, i] * gamma[k, i] + c[2, i] * f[k, t] + eta[k, i, t])
   #Calculate y.ctfl
   y.ctfl <- array(dim = c(N, T))
@@ -58,5 +58,5 @@ dataGeneratingProcess7 <- function(N, T0, T1){
 }
 # source("R/.Source.R")
 # 
-# dataGeneratingProcess1(40, 40, 10)
+# dataGeneratingProcess7(40, 40, 10)$y.actl
 # save(DGP1_data,file = "DGP1_data.Rdata")
